9c73d6da0e10de143a8e3735e25dc41d4359637e
cryptix <cryptix@riseup.net>
2010-06-04 18:01:13 +0200

    Two bugs in status.sh fixed.

--- sh/status.sh
+++ sh/status.sh
@@ -4,14 +4,15 @@
 while true;
 do
 	_date=$(date +"%a. %H:%M")
-	load=$(cat /proc/loadavg | cut -d' ' -f1-2)
+	_load=$(cat /proc/loadavg | cut -d' ' -f1-2)
 	_mem=$(free -m | grep 'Mem:' | cut -d' ' -f28)
 	_bat=$(acpitool | awk '/Battery #1/{cd=$4; perc=$5; time=$6} END {
 			if(cd == "discharging,")
 				printf("-] %s left",substr(time,0,5))
-			else
+			else {
 				sub("%,","%",perc);
 		   		printf("+] %s full",perc) 
+			}
 		} ') 
 
 	#echo "[$_load] [$_mem] [$_date]"

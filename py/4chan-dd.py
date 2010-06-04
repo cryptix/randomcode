95dd1c30963103e0a3f306ea83d2b4dc555db580
cryptix <cryptix@riseup.net>
2010-06-04 18:10:58 +0200

    4chan dumper added

---
+++ py/4chan-dd.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# 4chan thread dumper
+#
+# If you want to download all pictures of a thread this does the trick
+# its also threaded so be prepeard for a network burst
+
+
+from BeautifulSoup import BeautifulSoup
+from urllib2 import urlopen
+from os import fstat
+import threading
+import re
+
+class myDumper(threading.Thread):
+	"""Saves URL to File - in a thread"""
+	def __init__(self, url):
+		super(myDumper, self).__init__()
+		self.url = url
+	
+	def run(self):
+		pic = urlopen(self.url)
+		
+		if pic.getcode() == 200:
+				f = open(self.url.split('/')[-1] ,'w')
+				if fstat(f.fileno()).st_size == 0: f.write(pic.read())
+		print 'fetched: %s - %d' % (pic.geturl(), pic.getcode())
+	
+
+def dumpPics(url):
+	"""saves pictures (<img>) to curdir	"""
+	try:
+		html = urlopen(url).read()
+		soup = BeautifulSoup(html)
+		
+		# set() to filter out doubles
+		links = set([ l['href'] for l in soup('a', href=re.compile('http://images.4chan.org/d/src/'))]) # TODO: get correct board from url
+		
+		threads = []
+		for link in links: 
+			thread = myDumper(link)
+			thread.start()
+			threads.append(thread)
+		
+		for thread in threads: thread.join() # wait for thread to exit
+		
+		
+		return links
+		
+	except Exception, e:
+		raise e
+	else:
+		print "all well done"
+
+		
+if __name__ == '__main__':
+	import sys
+	
+	if len(sys.argv) != 2:
+		print("usage: %s url" % sys.argv[0])
+		sys.exit(1)
+	
+	my = dumpPics(sys.argv[1])

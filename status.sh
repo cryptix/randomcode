377738ebb7ce3550c77fb2f1d02ef4508b19f619
cryptix <cryptix@riseup.net>
2010-06-04 16:01:18 +0200

    status.sh - changed mem from used to free - cleand bat output - reindented

--- status.sh
+++ status.sh
@@ -1,14 +1,20 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
 #netbook version
 
 while true;
 do
-    _date=$(date +"%a. %H:%M")
-    _load=$(cat /proc/loadavg | cut -d' ' -f1-2)
-    _mem=$(free -m | grep 'Mem:' | cut -d' ' -f20)
-	_bat=$(acpitool | awk '/Battery #1/{cd=$4; perc=$5; time=$6} END{ if(cd == "discharging,") printf("-] %s left",substr(time,0,5)); else printf("+] %s full",perc) }')
+	_date=$(date +"%a. %H:%M")
+	load=$(cat /proc/loadavg | cut -d' ' -f1-2)
+	_mem=$(free -m | grep 'Mem:' | cut -d' ' -f28)
+	_bat=$(acpitool | awk '/Battery #1/{cd=$4; perc=$5; time=$6} END {
+			if(cd == "discharging,")
+				printf("-] %s left",substr(time,0,5))
+			else
+				sub("%,","%",perc);
+		   		printf("+] %s full",perc) 
+		} ') 
 
 	#echo "[$_load] [$_mem] [$_date]"
-		xsetroot -name "perf[$_load ${_mem}m] time[$_date] bat[$_bat]"
+	xsetroot -name "perf[$_load ${_mem}m] time[$_date] bat[$_bat]"
 	sleep 15
 done

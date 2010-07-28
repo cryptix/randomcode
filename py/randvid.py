6d2e2de5cd69fd9096dcd9cc13cbb433725e98b9
cryptix <cryptix@riseup.net>
2010-07-28 12:35:56 +0200

    randvid.py - wrapped fnmatch for list input and one-lined argument handling.

--- py/randvid.py
+++ py/randvid.py
@@ -4,16 +4,18 @@ import os, sys
 from fnmatch import fnmatch
 from random import choice
 
+def fnwrap(f, args):
+	return [fnmatch(f,arg) for arg in args]
 
 if __name__ == '__main__':
-	startdir = os.curdir
-	if len(sys.argv) > 1: startdir = sys.argv[1]
+	startdir = os.curdir if len(sys.argv) < 2 else sys.argv[1]
 
 	try:
 		vids = [curp+"/"+f for (curp, dirs, files) in os.walk(startdir)
 					for f in files
 					if not curp.endswith('.AppleDouble') # meh..
-					if fnmatch(f, '*.avi') or fnmatch(f,'*.mp4') or fnmatch(f,'*.wmv')]
+					if True in fnwrap(f, ['*.avi','*.mp4','*.wmv'])] #refactor to map
+
 		print(choice(vids))
 	except IndexError:
 		print("/dev/null")

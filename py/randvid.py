9ec7d9cf3c16235c60c6ab4b107add6f174c8561
cryptix <cryptix@riseup.net>
2010-09-15 20:01:21 +0200

    cleaned randvid and added mkv

--- py/randvid.py
+++ py/randvid.py
@@ -9,16 +9,18 @@ def fnwrap(f, args):
 
 if __name__ == '__main__':
 	startdir = os.curdir if len(sys.argv) < 2 else sys.argv[1]
+	cnt = 1 if len(sys.argv) < 3 else int(sys.argv[2])
 
 	try:
 		vids = [curp+"/"+f for (curp, dirs, files) in os.walk(startdir)
 					for f in files
-					if not curp.endswith('.AppleDouble') # meh..
-					if True in fnwrap(f, ['*.avi','*.mp4','*.wmv'])] #refactor to map
+					if True in fnwrap(f, ['*.avi','*.mp4','*.wmv', '*.mkv'])] #refactor to map
 
-		print(choice(vids))
+		new = [ choice(vids) for i in range(0,cnt)]
+		for f in set(new):
+			print(f)
+
 	except IndexError:
 		print("/dev/null")
-
 
 

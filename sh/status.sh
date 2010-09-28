a8678c3fbea8afa957e5bba8d2e7f3ad1aa249e8
cryptix <cryptix@riseup.net>
2010-09-28 12:58:24 +0200

    status.sh - cleaner mem and date

--- sh/status.sh
+++ sh/status.sh
@@ -3,9 +3,10 @@
 
 while true;
 do
-	_date=$(date +"%a. %H:%M")
+
+	_date=$(date "+%d.%b %H:%M")
 	_load=$(cat /proc/loadavg | cut -d' ' -f1-2)
-	_mem=$(free -m | grep 'Mem:' | cut -d' ' -f28)
+	_mem=$(awk '/^Cached/{total -= $2} /MemFree/{total += $2} END{printf("%.0f\n",total/1024)}' /proc/meminfo )
 	_bat=$(acpitool | awk '/Battery #1/{cd=$4; perc=$5; time=$6} END {
 			if(cd == "discharging,")
 				printf("-] %s left",substr(time,0,5))
@@ -15,7 +16,6 @@ do
 			}
 		} ') 
 
-	#echo "[$_load] [$_mem] [$_date]"
-	xsetroot -name "perf[$_load ${_mem}m] time[$_date] bat[$_bat]"
+	xsetroot -name "perf[$_load ${_mem}m] date[$_date] bat[$_bat]"
 	sleep 15
 done

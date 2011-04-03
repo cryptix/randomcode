a93ca4077c916ae1d18185c303b5a45ef207b162
cryptix <cryptix@riseup.net>
2011-04-03 18:08:32 +0200

    initial tree struct hacking

--- js/node/crawlr.js
+++ js/node/crawlr.js
@@ -1,3 +1,29 @@
+function limb(dir) {
+	this.name = dir;
+	this.leaves = [];	// ie files
+	this.crot = [];	// ie parent dirs
+	this.in = function(elem) {
+		var len = crot.length();
+		for(var i = 0; i < len; i++) {
+			if ( elem == crot[i] )
+				return crot[i];
+		}
+		return false;
+	}
+}
+
+
+function treeRoot(limb, path) {
+	var parts = path.split('/');
+	var plen = parts.length();
+	for(i=0; i < plen; i++) {
+	}
+}
+
+
+var treeRoot = new limb('root');
+
+
 var dir = process.ARGV[2];
 
 if(!dir)

5c60d49aa4e3759f244dd1c967ef01333d91d9e9
cryptix <cryptix@riseup.net>
2011-07-05 11:31:57 +0200

    more work on the crypto stuff

--- js/node/crypto/helper.js
+++ js/node/crypto/helper.js
@@ -1,47 +1,69 @@
-var crypto = require('crypto');
-var fs = require('fs');
+var crypto = require('crypto'),
+    fs = require('fs'),
+    assert = require('assert');
 
-var cstr = 'U2FsdGVkX1+eo8UipHtNThvPE77IsqU7ZG9BTElV4ZcqrEWLCghbvbtaUPuwEnPQ';
+// test strings
+var cstr = 'dpi/tu8JG0OoXXs1kF6Z5rO5lYSZdri3l4yeolWLea5RAmF/yO7nFdvYjd5pIvk4zm/g6AQWITS3sQs+hBz+dA==';
 var plain = 'ThisIsMyClearTEXT!\nThisIsMyClearTEXT!\nThisIsMyClearTEXT!\n';
 
-function decipher(cstr) {
-    var dec = crypto.createDecipher('aes-256-cbc', 'TestKey');
+var key = 'TestKey!123';
 
-    var plain = dec.update(cstr,'binary', 'utf8');
-    plain += dec.final('utf8');
+var scheme = 'aes256';
+var cryptEnc = 'base64';
+var clearEnc = 'utf8';
+
+function decipher(cstr) {
+    var dec = crypto.createDecipher(scheme, key);
+
+    var plain = '';
+
+    plain += dec.update(cstr, cryptEnc, clearEnc);
+    plain += dec.final(clearEnc);
 
     return plain;
 }
 
 function cipher(plain) {
-    var enc = crypto.createCipher('aes256', 'TestKey');
+    var enc = crypto.createCipher(scheme, key);
     
-    var cstr = enc.update(plain, 'utf8', 'binary');
-    cstr += enc.final('binary');
+    var cstr = '';
+
+    cstr += enc.update(plain, clearEnc, cryptEnc);
+    cstr += enc.final(cryptEnc);
     
    return cstr;
 }
 
 
+//console.log(decipher(cstr));
+//console.log(cipher(plain));
+
+assert.equal(decipher(cipher(plain)), plain);
+assert.equal(cipher(decipher(cstr)), cstr);
+
+/*
+var f = fs.createWriteStream('help/test1');
+f.write(cipher(plain));
+*/
+
+/*
 function decFromFile (fname) {
-    var s = fs.ReadStream(fname, {encoding: 'binary'});
-    var dec = crypto.createDecipher('aes256', 'TestKey');
+    var s = fs.ReadStream(fname, {encoding: cryptEnc});
+    var dec = crypto.createDecipher(scheme, key);
         
     var plain = '';
     
     s.on('data', function(d) {
-        var str = new Buffer(d).toString('base64');
-        plain += dec.update(str, 'base64', 'utf8');
+        plain += dec.update(d, cryptEnc, clearEnc);
     });
 
     s.on('end', function() {
-        plain += dec.final('utf8');
-        console.log(plain);
+        plain += dec.final(clearEnc);
+        return plain;
     });
 }
 
-//console.log(decipher(cipher(plain)));
-
 // created with
 // echo -e 'ThisIsMyClearTEXT!\nThisIsMyClearTEXT!\nThisIsMyClearTEXT!\n' | openssl enc -aes256 -e -k 'TestKey' -out ramsch
-decFromFile('ramsch');
+//decFromFile('ramsch');
+*/

519a12cfa6e8841209ab6dbb306a95f66c6c4951
Henry <cryptix@riseup.net>
2011-07-11 23:03:54 +0200

    little httpd helper

---
+++ js/node/staticHttpd.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var connect = require('connect')
+  , port = 3000
+  , path = process.cwd();
+
+connect(
+  connect.static(path)
+).listen(port);
+
+console.log('Listening at [%s] on port %d', path, port);
+

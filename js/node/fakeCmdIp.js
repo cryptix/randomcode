8b72d18a6b78ac93f209e898a4d7b7d473a985a9
Henry <cryptix@riseup.net>
2011-08-05 15:37:07 +0200

    Fake command interpreter for local dev

---
+++ js/node/fakeCmdIp.js
@@ -0,0 +1,59 @@
+var net = require('net');
+
+
+var con = net.createConnection(9999, 'localhost');
+
+con.on('connect', function() {
+  con.write('S>1=2\rR>0x00000001\r');
+});
+
+var levelLeft = 0;
+var levelRight = 0;
+
+con.on('data', function(data) {
+  data = data.toString().split('\r')[0];
+  var cmd = data.split(' ')[0];
+  var par = data.split(' ')[1];
+  var ret = '';
+
+  switch(cmd) {
+    case '11': // freq
+      ret = 'S>11=' + par;
+      break;
+    case '12': // level left
+      levelLeft += Number(par);
+      if(levelLeft > 1300) {
+        levelLeft = 1300;
+      } else if(levelLeft < -100) {
+        levelLeft = -100;
+      }
+      ret = 'S>13=' + levelLeft;
+      break;
+    case '13': // level right
+      levelRight += Number(par);
+      if(levelRight > 1300) {
+        levelRight = 1300;
+      } else if(levelRight < -100) {
+        levelRight = -100;
+      }
+      ret = 'S>12=' + levelRight;
+      break;
+    case '14':
+      ret = 'S>1=' + par;
+      break;
+    case '15': // side
+      ret = 'S>2=' + par;
+      break;
+    case '16': // modulation
+      ret = 'S>3=' + par;
+      break;
+    case '19': // level lock
+      ret = 'S>7=' + par;
+      break;
+  }
+
+  console.log('cmd[%s] par[%s] ret[%s]', cmd, par, ret);
+
+  ret += '\rR>0x00000001\r';
+  con.write(ret);
+});

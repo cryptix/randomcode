a47fe3b20c628cd5f72a1a39b41f48cac5b07325
Henry <cryptix@riseup.net>
2013-01-15 20:35:53 +0100

    updated terMenu and dwm status script

--- sh/terMenu.sh
+++ sh/terMenu.sh
@@ -2,33 +2,41 @@
 
 term=urxvtc
 
-CACHE=${XDG_CACHE_HOME:-"$HOME/.cache"}/dmenu_run
+cachedir=${XDG_CACHE_HOME:-"$HOME/.cache"}
+if [ -d "$cachedir" ]; then
+	cache=$cachedir/dmenu_run
+else
+	cache=$HOME/.dmenu_cache # if no xdg dir, fall back to dotfile in ~
+fi
 (
-  IFS=:
-  if test "`ls -dt $PATH "$CACHE" 2> /dev/null | sed 1q`" != "$CACHE"; then
-    mkdir -p "`dirname "$CACHE"`" && lsx $PATH | sort -u > "$CACHE"
-  fi
-)
+	IFS=:
+	if stest -dqr -n "$cache" $PATH; then
+		stest -flx $PATH | sort -u > "$cache"
+	fi
+) 
 
 choice=`awk -F'"' '/"\)/{print $2}' ~/bin/terMenu.sh | dmenu -p 'start:'`
-case $choice  in
+case $choice in
+  "open")
+    dir=`lsof  -d cwd -Fn | awk '/^n/{sub("n","" ); print }' | sort -u | dmenu -p "open:"`
+    cd "$dir" && exec $term
+  ;;
+
   "ssh")
     host=`awk '/^Host/{print $2}' ~/.ssh/config | dmenu -p 'ssh:'` && exec $term -e ssh $host
   ;;
 
-  "hist")
-  ;;
 
   "root")
     exec $term -e su -
   ;;
 
   "window")
-    cmd=`dmenu -p 'Window:' < "$CACHE"` && exec $cmd
+    cmd=`dmenu -p 'Window:' < "$cache"` && exec $cmd
   ;;
 
   "term")
-    cmd=`dmenu -p 'Term:' < "$CACHE"` && exec $term -e $cmd
+    cmd=`dmenu -p 'Term:' < "$cache"` && exec $term -e $cmd
   ;;
 
   *)

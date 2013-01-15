70c0231007fba7f311bb59e7fde84e1724e287b3
Henry <cryptix@riseup.net>
2013-01-16 00:50:34 +0100

    Project Setup

---
+++ js/socialBook/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+app.configure(function() {
+  app.set('view engine', 'jade');
+  app.use(express.static(__dirname + '/public'));
+});
+
+
+app.get('/', function(req, res) {
+  res.render('index.jade', {layout: false});
+});
+
+app.listen(8080);

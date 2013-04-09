56771759e56eeacb28d3be79b95cd09bfe6592e7
Henry <cryptix@riseup.net>
2013-04-09 16:09:19 +0200

    staticHttpd.js - connect with static and logging

--- js/node/staticHttpd.js
+++ js/node/staticHttpd.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
-var connect = require('connect')
-  , port = 3000
-  , path = process.cwd();
+var connect = require('connect'),
+	port = 3000,
+	path = process.cwd();
 
-connect(
-  connect.static(path)
-).listen(port);
+connect()
+  .use(connect.logger('dev'))
+  // .use(connect.logger(':status - :method :url'))
+  .use(connect.static(path))
+  .listen(port);
 
 console.log('Listening at [%s] on port %d', path, port);
 

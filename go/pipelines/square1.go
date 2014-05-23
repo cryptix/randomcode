7bf41ee1a8665dec7edf76c8a671fdc08958744b
Henry <cryptix@riseup.net>
2014-05-23 17:11:06 +0200

    some channel stuff

---
+++ go/pipelines/square1.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func merge(cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
+
+	// Start an output goroutine for each input channel in cs.  output
+	// copies values from c to out until c is closed, then calls wg.Done.
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	// Start a goroutine to close out once all the output goroutines are
+	// done.  This must start after the wg.Add call.
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+
+	in := gen(2, 3, 5, 1, 23, 1, 3, 3, 4, 5, 6, 7)
+
+	c1 := sq(in)
+	c2 := sq(sq(in))
+
+	// Consume the output.
+	for n := range merge(c1, c2) {
+		fmt.Println(n)
+	}
+
+}
